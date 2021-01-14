25a8ba04c501774fcf98520e3d68a2905d0585d8
Keryanne <47926567+Keryanne@users.noreply.github.com>
2021-01-14 14:27:42 +0100

    vers php

--- init/bas-page.php
+++ init/bas-page.php
@@ -1,78 +1,9 @@
-<footer>
-    <div class="top container">
-        <div class="footer-logo">
-            <img src="https://www.hotel-neptune.fr/images/favicon.png" alt="Hotel Neptune" title="Hotel Neptune"/>
-            <div class="footer-logo-text">
-                <span class="hotel-name">Hotel Neptune</span>
-            </div>
-        </div>
-        <nav id="footer-menu">
-            <ul >                          
-                <li  data-element-id="12">
-                    <a href="Projet-PHP/index.html">
-                    Connexion
-                    </a>
-                </li>                            
-                <li  data-element-id="13">
-                    <a href="file:///C:/Users/Z30-B/Documents/GitHub/Projet-PHP/index.html">
-                    Inscription
-                    </a>
-                </li>                            
-                <li  data-element-id="10">
-                    <a href="file:///C:/Users/Z30-B/Documents/GitHub/Projet-PHP/index.html">
-                    Organisations
-                    </a>
-                </li>                            
-                <li  data-element-id="11">
-                    <a href="https://www.google.fr/maps/place/H%C3%B4tel+Neptune/@43.545289,3.973821,17z/data=!3m1!4b1!4m2!3m1!1s0x12b6bb3f6f499f5b:0xa4273d08bd579ecb?hl=fr">
-                    Localisation
-                    </a>
-                </li>                                                                                      
-                <li  data-element-id="9">
-                    <a href="file:///C:/Users/Z30-B/Documents/GitHub/Projet-PHP/index.html">
-                    Contact
-                    </a>
-                </li>                            
-                <li  data-element-id="17">
-                    <a href="file:///C:/Users/Z30-B/Documents/GitHub/Projet-PHP/index.html">
-                    Cookies
-                    </a>
-                </li>                            
-                <li  data-element-id="17">
-                    <a href="file:///C:/Users/Z30-B/Documents/GitHub/Projet-PHP/index.html">
-                    Mentions légales
-                    </a>
-                </li>                            
-            </ul>
-        </nav>        
-        <nav id="footer-menu-mobile">
-            <a class="location" href="https://www.google.fr/maps/place/H%C3%B4tel+Neptune/@43.545289,3.973821,17z/data=!3m1!4b1!4m2!3m1!1s0x12b6bb3f6f499f5b:0xa4273d08bd579ecb?hl=frs"><span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span></a>
-            <a class="phone" href="tel:+33467508800"><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span></a>
-        </nav>
+<footer class="footer text-faded text-center py-5">
+    <div class="container">
+      <p class="m-0 small">Copyright &copy; 2020</p>
     </div>
-    <div class="bottom-row">
-        <div class="container">
-            <div class="bottom">
-                <form id="newsletter-subscribe" action="" method="post">
-                    <p class="intro">Soyez le premier à être au courant des offres et nouveautés.</p>
-                    <div class="form-content">
-                        <div class="input-group">
-                            <input type="email" class="form-control" name="email" placeholder="E-mail">
-                            <a href="javascript:{}" onclick="document.getElementById('newsletter-subscribe').submit();" class="input-group-addon">Envoyez <span class="glyphicon glyphicon-chevron-right"></span></a>
-                        </div>
-                    </div>
-                </form>
-                <div id="partners">
-                    <ul>
-                                    <li class="partner">
-                                
-                                    <img src="https://www.hotel-neptune.fr/images/logos/theoriginals_s.png" alt="Costa Brava Hotels de Luxe"/>
-                                
-                            </li>
-                            </ul>
-                </div>            
-            </div>
-        </div>
-    </div>
+  </footer>
 
-</footer>
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
