c8c1e51b4f62260ba8b8d79f563d67cc6ce8e196
Anais-Simon <58404134+Anais-Simon@users.noreply.github.com>
2021-01-15 11:31:06 +0100

    Up

---
+++ pages-Client/verification.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+if(isset($_POST['mail']) && isset($_POST['mot_de_passe']))
+{
+    // connexion à la base de données
+    $db_username = 'root';
+    $db_password = '';
+    $db_name     = 'neptune';
+    $db_host     = 'localhost';
+    $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
+           or die('could not connect to database');
+    
+    // on applique les deux fonctions mysqli_real_escape_string et htmlspecialchars
+    // pour éliminer toute attaque de type injection SQL et XSS
+    $mail = mysqli_real_escape_string($db,htmlspecialchars($_POST['mail'])); 
+    $mot_de_passe = mysqli_real_escape_string($db,htmlspecialchars($_POST['mot_de_passe']));
+    
+    if($mail !== "" && $mot_de_passe !== "")
+    {
+        $requete = "SELECT count(*) FROM clients where 
+              mail = '".$mail."' and mot_de_passe = '".$mot_de_passe."' ";
+        $exec_requete = mysqli_query($db,$requete);
+        $reponse      = mysqli_fetch_array($exec_requete);
+        $count = $reponse['count(*)'];
+        if($count!=0) // nom d'utilisateur et mot de passe correctes
+        {
+           $_SESSION['mail'] = $mail;
+           header('Location: /Projet-PHP/pages-Admin/index-admin.php');
+        }
+        else
+        {
+           header('Location: login.php?erreur=1'); // utilisateur ou mot de passe incorrect
+        }
+    }
+    else
+    {
+       header('Location: login.php?erreur=2'); // utilisateur ou mot de passe vide
+    }
+}
+else
+{
+   header('Location: login.php');
+}
+mysqli_close($db); // fermer la connexion
+?>
