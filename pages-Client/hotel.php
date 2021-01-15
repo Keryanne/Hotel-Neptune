60e3613df895ed4ae2c4be9b276ae427ca203f26
Keryanne <47926567+Keryanne@users.noreply.github.com>
2021-01-15 10:53:25 +0100

    up

--- pages-Client/hotel.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("init/init.php");
-require_once("init/haut-page.php");
-?>
-
-<?php 
-require_once("init/bas-page.php");
-?>
--- pages-Client/reservation.php
+++ pages-Client/reservation.php
@@ -1,8 +1,8 @@
 <?php
-require_once("init/init.php");
-require_once("init/haut-page.php");
+require_once("../init/init.php");
+require_once("../init/haut-page.php");
 ?>
 
 <?php 
-require_once("init/bas-page.php");
+require_once("../init/bas-page.php");
 ?>
