f919ef8ea3f12414a9fa4442525abbbb36883df7
Axel <74728599+Axeuul@users.noreply.github.com>
2021-01-15 02:44:39 +0100

    reservation et utilisateur

--- pages-Admin/admin-utilisateur.php
+++ pages-Admin/admin-utilisateur.php
@@ -6,42 +6,56 @@ require_once("../init/haut-page.php");
 
 
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <title>Récap</title>
-</head>
-<body>
-    <form action="">
-        <div class="row">
-            <h1 style=" font-size: 20px; color: gray;">Trier par</h1>
-            <div class="form-group col-md-2">
-                <label for="inputState">Civilité</label>
-                <select id="inputState" class="form-control">
-                  <option selected>...</option>
-                  <option value="Monsieur">Monsieur</option>
-                  <option value="Madame">Madame</option>
-                  <option value="Mademoiselle">Mademoiselle</option>
-                </select>
-            </div>
-            <div class="form-group col-md-2">
-              <label for="inputState">Pays ID</label>
-              <select id="inputState" class="form-control">
-                  <option selected>...</option>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-              </select>
-      </div>
-                
-            
-        </div>
+<!DOCTYPE html><?php
+require_once("../init/init.php");
+require_once("../init/haut-page-admin.php");
+?>
 
-        <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+<link rel="stylesheet" href="<?php echo RACINE_SITE; ?>pages-Client/css/bootstrap.min.css">
+<link rel="stylesheet" href="<?php echo RACINE_SITE; ?>pages-Client/css/bootstrap.css">
+
+
+
+<div style="background-color:white;">
+<form action="" >
+<div style="margin-left:20px; padding-top:20px;">
+  
+      <h2 style=" font-size: 20px; color: gray;">Trier par</h2>
+    <div class="form-row" style="display:flex; flex-direction:row;"> 
+        <div class="form-group col-md-2" style="margin-right:10px;">
+            <label for="inputState">Capacité</label>
+            <select id="inputState" class="form-control">
+              <option selected>...</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+            </select>
+        </div>
+        <div class="form-group col-md-2" style="margin-right:10px;>
+            <label for="inputState">Etage</label>
+            <select id="inputState" class="form-control">
+                <option selected>...</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+            </select>
+        </div>
+            
+        <div class="form-group col-md-2" style="margin-right:10px;>
+                <label for="inputState">Exposition</label>
+                <select id="inputState" class="form-control">
+                    <option selected>...</option>
+                    <option value="Port">Port</option>
+                    <option value="Rempart">Rempart</option>
+                </select>
+        </div>
+    </div>
+  </div>     
+        
+        <br>
+        <br>
+
+        <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups" style="justify-content:space-around;">
             <div class="btn-group mr-2" role="group" aria-label="First group">
               <button type="button" class="btn btn-secondary">Ok</button>
             </div>
@@ -49,19 +63,20 @@ require_once("../init/haut-page.php");
               <button type="button" class="btn btn-secondary">Annuler</button>
             </div>
           </div>
+
     </form>
+    <br>
    
-    <table class="table table-hover">
+    <table class="table table-hover" style="text-align:center; margin-bottom:0;">
         <thead>
           <tr>
-            <th scope="col">ID</th>
+            <th scope="col">id</th>
             <th scope="col">Civilité</th>
             <th scope="col">Nom</th>
             <th scope="col">Prénom</th>
             <th scope="col">Adresse</th>
-            <th scope="col">Code postal</th>
+            <th scope="col">Code Postal</th>
             <th scope="col">Ville</th>
-            <th scope="col">Pays ID</th>
             <th scope="col">Modification</th>
             <th scope="col">Suppression</th>
           </tr>
@@ -69,21 +84,14 @@ require_once("../init/haut-page.php");
         <tbody>
           <tr>
             <th scope="row">1</th>
-            <td>Madame</td>
-            <td>Morin</td>
-            <td>Joélle</td>
-            <td>34 rue Saint Denis</td>
-            <td>67000</td>
-            <td>Strasbourg</td>
-            <td>1</td>
-            <td><a href=""><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16" style="color : black;">
-              <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-              <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-            </svg></a></td>
-            <td><a href=""><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16" style="color : black;">
-  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-</svg></a></td>
+            <td>Mademoiselle</td>
+            <td>Dumas</td>
+            <td>Sandrine</td>
+            <td>15 allée des Tilleuls</td>
+            <td>75010</td>
+            <td>Paris</td>
+            <td style="width:15%;"><a href="<?php echo RACINE_SITE; ?>pages-admin/modif-chambre.php"><img style="width: 50px;" src="https://cdn.icon-icons.com/icons2/1518/PNG/512/pencilmono_105944.png" alt=""></a></td>
+            <td style="width:15%;"><a href=""><img style="width: 50px;" src="https://cdn.icon-icons.com/icons2/1518/PNG/512/crossmono_105892.png" alt=""></a></td>
           </tr>
           <tr>
             <th scope="row">2</th>
@@ -93,48 +101,24 @@ require_once("../init/haut-page.php");
             <td>North avenue 44</td>
             <td>TW9 3</td>
             <td>Kew</td>
-            <td>2</td>
-            <td><a href=""><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16" style="color : black;">
-              <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-              <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-            </svg></a></td>            
-            
-            <td><a href=""><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16" style="color : black;">
-  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-</svg></a></td>
-
+            <td><a href="<?php echo RACINE_SITE; ?>pages-admin/modif-chambre.php"><img style="width: 50px;" src="https://cdn.icon-icons.com/icons2/1518/PNG/512/pencilmono_105944.png" alt=""></a></td>
+            <td><a href=""><img style="width: 50px;" src="https://cdn.icon-icons.com/icons2/1518/PNG/512/crossmono_105892.png" alt=""></a></td>
           </tr>
           <tr>
             <th scope="row">3</th>
-            <td>Mademoiselle</td>
-            <td>Dumas</td>
-            <td>Sandrine</td>
-            <td>5 allée des tilleuls</td>
-            <td>75010</td>
-            <td>Paris</td>
-            <td>1</td>
-            <td><a href=""><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16" style="color : black;">
-              <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-              <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-            </svg></a></td>
-            
-            <td><a href=""><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16" style="color : black;">
-  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-</svg></a></td>
+            <td>Madame</td>
+            <td>Morin</td>
+            <td>Joélle</td>
+            <td>34 rue Saint denis</td>
+            <td>67000</td>
+            <td>Strasbourg</td>
+            <td><a href="<?php echo RACINE_SITE; ?>pages-admin/modif-chambre.php"><img style="width: 50px;" src="https://cdn.icon-icons.com/icons2/1518/PNG/512/pencilmono_105944.png" alt=""></a></td>
+            <td><a href=""><img style="width: 50px;" src="https://cdn.icon-icons.com/icons2/1518/PNG/512/crossmono_105892.png" alt=""></a></td>
           </tr>
         </tbody>
       </table>
-</body>
-</html> 
-
-
-
-
-
-
-
+</div>
+ 
 <?php 
 require_once("../init/bas-page.php");
 ?>
