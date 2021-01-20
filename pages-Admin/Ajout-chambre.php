272da07d030085ee2cca77be4c07de90835baeb6
Keryanne <47926567+Keryanne@users.noreply.github.com>
2021-01-20 15:52:42 +0100

    Update Ajout-chambre.php

--- pages-Admin/Ajout-chambre.php
+++ pages-Admin/Ajout-chambre.php
@@ -5,7 +5,26 @@ require_once("../init/haut-page-admin.php");
 
 <?php
 
-$dchambre = executeRequete("SELECT DISTINCT * FROM chambres INNER JOIN tarifs ON chambres.tarif_id = tarifs.tarif_id"); 
+if(!empty($_POST))
+{   // debug($_POST);
+  $photo_bdd = ""; 
+    if(!empty($_FILES['photo']['name']))
+    {   // debug($_FILES);
+        $nom_photo = $_POST['reference'] . '_' .$_FILES['photo']['name'];
+        $photo_bdd = RACINE_SITE . "photo/$nom_photo";
+        $photo_dossier = $_SERVER['DOCUMENT_ROOT'] . RACINE_SITE . "/image/$nom_photo"; 
+        copy($_FILES['photo']['tmp_name'],$photo_dossier);
+    }
+    foreach($_POST as $indice => $valeur)
+    {
+        $_POST[$indice] = htmlEntities(addSlashes($valeur));
+    }
+    executeRequete("INSERT INTO chambres (id_chambre, Nom_chambre, capacite, exposition, douche, etage, tarif_id, photo) values ('', '$_POST[Nom_chambre]', '$_POST[capacite]', '$_POST[exposition]', '$_POST[douche]', '$_POST[etage]', '$_POST[tarif_id]','$photo_bdd')");
+    
+    echo '<div class="validation">Le produit a été ajouté</div>';
+}
+
+//$dchambre = executeRequete("SELECT DISTINCT * FROM chambres INNER JOIN tarifs ON chambres.tarif_id = tarifs.tarif_id"); 
 
 ?>
 
@@ -15,18 +34,18 @@ $dchambre = executeRequete("SELECT DISTINCT * FROM chambres INNER JOIN tarifs ON
         <br>
         <div class="row">
             <div class="col">
-              <input type="text" class="form-control" placeholder="Nom de la chambre" value='$dchambre['Nom_chambre']'>
+              <input type="text" class="form-control" placeholder="Nom de la chambre" name="Nom_chambre">
             </div>
             <div class="col">
-              <input type="text" class="form-control" placeholder="Prix" value='$dchambre['prix']'>
+              <input type="text" class="form-control" placeholder="Prix" name="tarif_id">
             </div>
         </div>
         <br>
         <div class="form-group col-md-4">
             <label for="inputState">Capacité</label>
-            <select id="inputState" class="form-control">
+            <select id="inputState" name="capacite" class="form-control">
               <option selected>...</option>';
-              for($i = 1;$i <= 3;$i++)
+              for($i = 1;$i <= 4;$i++)
               {
                   echo '<option value="'.$i.'">'.$i.'</option>';
               }
@@ -35,7 +54,7 @@ $dchambre = executeRequete("SELECT DISTINCT * FROM chambres INNER JOIN tarifs ON
           <br>
         <div class="form-group col-md-4">
         <label for="inputState">Etage</label>
-        <select id="inputState" class="form-control">
+        <select id="inputState" name="etage" class="form-control">
             <option selected>...</option>';
             for($i = 1;$i <= 3;$i++)
             {
@@ -46,27 +65,20 @@ $dchambre = executeRequete("SELECT DISTINCT * FROM chambres INNER JOIN tarifs ON
         <br>
         <div class="form-group col-md-4">
             <label for="inputState">Exposition</label>
-            <select id="inputState" class="form-control">
+            <select id="inputState" name="exposition" class="form-control">
                 <option selected>...</option>';
-                if($dchambre['exposition'] == 'port')
-                {
                   echo '<option>Port</option>';
-                }
-                else
-                {
                   echo '<option>Rempart</option>';
-                }
             echo '</select>
             </div>
         <br>
         <div class="form-group">
             <label for="exampleFormControlFile1">Photos</label><br>
-            <input type="file" class="form-control-file" id="exampleFormControlFile1">
+            <input type="file" class="form-control-file" id="exampleFormControlFile1" name="photo">
         </div>
         <br>
         <button type="submit" class="btn btn-primary">Ajouté</button>
-        <!--<button type="submit" class="btn btn-primary">Enregistré</button>    Pour la page de modification-->
     </form>';
-<?php 
+
 require_once("../init/bas-page.php");
 ?>
